fe5eecdfb4eb56c1b8a8c23a59d779603079fe21
shgur7236 <shgurtns7236@naver.com>
2022-06-22 22:58:53 +0900

    Security

---
+++ src/main/java/me/silvernine/jwttutorial/config/SecurityConfig.java
@@ -0,0 +1,18 @@
+package me.silvernine.jwttutorial.config;
+
+import org.springframework.security.config.annotation.web.HttpSecurityBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http
+                .authorizeRequests() //HttpServletRequest를 사용하는 요청들에 대한 접근제한 설정
+                .antMatchers("/api/hello").permitAll() //"~"에 대한 요청은 인증없이 접근을 허용
+                .anyRequest().authenticated();
+    }
+}
