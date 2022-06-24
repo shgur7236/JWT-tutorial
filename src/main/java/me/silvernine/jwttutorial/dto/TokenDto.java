840029c7a1458e69d7f184cedd0d36f4e0e9d27c
shgur7236 <shgurtns7236@naver.com>
2022-06-24 14:59:29 +0900

    Token

---
+++ src/main/java/me/silvernine/jwttutorial/dto/TokenDto.java
@@ -0,0 +1,13 @@
+package me.silvernine.jwttutorial.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TokenDto {
+
+    private String token;
+}
