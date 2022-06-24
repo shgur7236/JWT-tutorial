a2d5af53cd8ea661fa3885faeac45dd139470ef5
shgur7236 <shgurtns7236@naver.com>
2022-06-24 15:53:43 +0900

    login 부분 개발

---
+++ src/main/java/me/silvernine/jwttutorial/controller/AuthController.java
@@ -0,0 +1,54 @@
+package me.silvernine.jwttutorial.controller;
+
+import me.silvernine.jwttutorial.dto.LoginDto;
+import me.silvernine.jwttutorial.dto.TokenDto;
+import me.silvernine.jwttutorial.jwt.JwtFilter;
+import me.silvernine.jwttutorial.jwt.TokenProvider;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api")
+public class AuthController {
+    private final TokenProvider tokenProvider;
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
+
+    public AuthController(TokenProvider tokenProvider, AuthenticationManagerBuilder authenticationManagerBuilder) {
+        this.tokenProvider = tokenProvider;
+        this.authenticationManagerBuilder = authenticationManagerBuilder;
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<TokenDto> authorize(@Valid @RequestBody LoginDto loginDto) {
+
+        //LoginDto의 username, password로 UsernamePasswordAuthenticationToken authenticationToken = 생성
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(loginDto.getUsername(), loginDto.getPassword());
+
+        //authenticationtoken를 이용해서 authentication객체을 생성하고 authenticate메서드가 실행될 때 loadUserByName메서드가 실행된다
+        //생성된 authentication객체는 SecurityContext에 저장
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        //authentication객체를 createToken을 통해 Jwt Token을 생
+        String jwt = tokenProvider.createToken(authentication);
+
+        //Response Header에 추가
+         HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add(JwtFilter.AUTHORIZATION_HEADER, "Bearer " + jwt);
+
+        //ResponseBody에도 넣어주고 리턴
+        return new ResponseEntity<>(new TokenDto(jwt), httpHeaders, HttpStatus.OK);
+    }
+}
