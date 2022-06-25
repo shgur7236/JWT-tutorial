8169fced412266c40967562b5d07daaa521e075f
shgur7236 <shgurtns7236@naver.com>
2022-06-25 16:53:01 +0900

    회원가입 서비스

---
+++ src/main/java/me/silvernine/jwttutorial/service/UserService.java
@@ -0,0 +1,55 @@
+package me.silvernine.jwttutorial.service;
+import java.util.Collections;
+import java.util.Optional;
+import me.silvernine.jwttutorial.dto.UserDto;
+import me.silvernine.jwttutorial.entity.Authority;
+import me.silvernine.jwttutorial.entity.User;
+import me.silvernine.jwttutorial.repository.UserRepository;
+import me.silvernine.jwttutorial.util.SecurityUtil;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Transactional
+    public User signup(UserDto userDto) {
+        if (userRepository.findOneWithAuthoritiesByUsername(userDto.getUsername()).orElse(null) != null) {
+            throw new RuntimeException("이미 가입되어 있는 유저입니다.");
+        }
+
+        Authority authority = Authority.builder()
+                .authorityName("ROLE_USER")
+                .build();
+
+        User user = User.builder()
+                .username(userDto.getUsername())
+                .password(passwordEncoder.encode(userDto.getPassword()))
+                .nickname(userDto.getNickname())
+                .authorities(Collections.singleton(authority))
+                .activated(true)
+                .build();
+
+        return userRepository.save(user);
+    }
+
+    //username을 기준으로 정보를 가져온다
+    @Transactional(readOnly = true)
+    public Optional<User> getUserWithAuthorities(String username) {
+        return userRepository.findOneWithAuthoritiesByUsername(username);
+    }
+
+    //SecurityContext에 저장된 username의 정보만 가져온다
+    @Transactional(readOnly = true)
+    public Optional<User> getMyUserWithAuthorities() {
+        return SecurityUtil.getCurrentUsername().flatMap(userRepository::findOneWithAuthoritiesByUsername);
+    }
+}
